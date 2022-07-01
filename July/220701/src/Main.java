8cdcc0a9c3e2f5e6e8159af8e0e2c2ddb3e95cac
Damgom <wisewish1@gmail.com>
2022-07-01 20:16:53 +0900

    220701

--- July/220701/src/Main.java
+++ July/220701/src/Main.java
@@ -1,5 +1,47 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+//백준 10816 숫자카드2 실버4
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int N = input.nextInt();
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = input.nextInt();
+        }
+        Arrays.sort(arr);
+        int M = input.nextInt();
+        for(int i = 0; i < M; i++){
+            int key = input.nextInt();
+            sb.append(upperBound(arr,key) - lowerBound(arr,key)).append(' ');
+        }
+        System.out.println(sb);
+    }
+    public static int lowerBound(int[] arr, int key){
+        int low = 0;
+        int high = arr.length;
+        while (low < high){
+            int mid = (low + high) / 2;
+
+            if(key <= arr[mid]){
+                high = mid;
+            }
+            else low = mid + 1;
+        }
+        return low;
+    }
+    public static int upperBound(int[] arr, int key){
+        int low = 0;
+        int high = arr.length;
+        while(low < high){
+            int mid = (low + high) / 2;
+            if(key < arr[mid]){
+                high = mid;
+            }
+            else low = mid + 1;
+        }
+        return low;
     }
 }
