fc516e4266f7500475af7b053d0ab88d57363089
Damgom <wisewish1@gmail.com>
2022-08-30 23:17:32 +0900

    220830

---
+++ August/_220830/src/Main.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        StringTokenizer st = new StringTokenizer(s);
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int count = 0;
+        int comp = 0;
+        for(int i = 0; i < N; i++){
+            if(comp == arr[i]){
+                count++;
+                comp++;
+                if(comp==3){
+                    comp = 0;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
