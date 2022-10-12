5e7a46b46a35a86ef2620e17450b29653e087d83
Damgom <wisewish1@gmail.com>
2022-10-12 22:06:16 +0900

    221012

---
+++ October/_221012/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> result = new ArrayList<>();
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int L = Integer.parseInt(st.nextToken());
+            int P = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+
+            if(L == 0 && P == 0 && V == 0) break;
+            result.add(((V / P) * L) + Math.min((V % P), L));
+        }
+        for(int i = 1; i <= result.size(); i++) {
+            System.out.println("Case " + i + ": " + result.get(i-1));
+        }
+    }
+}
