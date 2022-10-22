3d2f8465bd1cb0842ea0217f9d990236ea1adaa2
Damgom <wisewish1@gmail.com>
2022-10-22 21:27:04 +0900

    221022

---
+++ October/_221022/src/Main.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] t = new int[n];
+        int[] p = new int[n];
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] dp = new int[n + 1];
+        for(int i = 0; i < n; i++) {
+            //일하는날
+            int day = i + t[i];
+            if(day <= n) {
+                dp[day] = Math.max(dp[day], dp[i] + p[i]);
+            }
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+        }
+        System.out.println(dp[n]);
+    }
+}
