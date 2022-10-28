ff089e39501cca009c54b0d6d8a36fd2d9b57d76
Damgom <wisewish1@gmail.com>
2022-10-28 10:46:18 +0900

    221028

---
+++ October/_221028/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String num = br.readLine();
+        char[] arr = num.toCharArray();
+
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for(int i = arr.length -1; i >= 0; i--) {
+            sb.append(arr[i]);
+        }
+        System.out.println(sb);
+    }
+}
