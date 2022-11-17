6cca2c3869c766099de42f87344239580ee959fb
Damgom <wisewish1@gmail.com>
2022-11-17 20:52:27 +0900

    221117

---
+++ November/_221117/Boj_2740.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj_2740 {
+    public static void main(String[] args) throws IOException {
+        /*
+        행렬 곱셈
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] aArr = new int[n][m];
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++) {
+                aArr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] bArr = new int[m][k];
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < k; j++) {
+                bArr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < k; j++) {
+                int sum = 0;
+                for(int l = 0; l < m; l++) {
+                    sum += aArr[i][l] * bArr[l][j];
+                }
+                sb.append(sum).append(' ');
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+}
