8fa65853a979a77c1e73b422bd823cd4e472afec
Damgom <wisewish1@gmail.com>
2022-12-31 18:21:45 +0900

    221231 string

---
+++ December/Boj_5525.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Boj_5525 {
+    public static void main(String[] args) throws IOException {
+        /*
+        boj_5525, IOIOI
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+//        String temp = "I";
+//        for (int i = 0; i < n; i++) {
+//            temp += "OI";
+//        }
+//        int count = 0;
+//        for (int i = 0; i < m - temp.length(); i++) {
+//            boolean flag = true;
+//            for (int j = 0; j < temp.length(); j++) {
+//                if (temp.charAt(j) != s.charAt(i + j)) {
+//                    flag = false;
+//                    break;
+//                }
+//            }
+//            if (flag) {
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'I') {
+                list.add(i);
+            }
+        }
+        int count = 0;
+        int answer = 0;
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i + 1) - list.get(i) == 2) {
+                count++;
+            }else {
+                count = 0;
+            }
+            if (count >= n) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
