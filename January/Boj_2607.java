0e32504dbb24c66b1d86071c1c282cda4b6ea0ba
Damgom <wisewish1@gmail.com>
2023-01-19 21:01:17 +0900

    230119 implementation String

---
+++ January/Boj_2607.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_2607 {
+    public static void main(String[] args) throws IOException {
+        /**
+         * @Boj_2607 비슷한 단어
+         * @Implementation,String
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String target = br.readLine();
+        int[] alphabet = new int[26];
+        for (int i = 0; i < target.length(); i++) {
+            int temp = target.charAt(i) - 'A';
+            alphabet[temp]++;
+        }
+        int answer = 0;
+        for (int i = 0; i < n-1; i++) {
+            int[] temp = alphabet.clone();
+            String compare = br.readLine();
+            int count = 0;
+            for (int j = 0; j < compare.length(); j++) {
+                if(temp[compare.charAt(j) - 'A'] != 0) {
+                    count++;
+                    temp[compare.charAt(j) - 'A']--;
+                }
+            }
+            if (target.length() - 1 == compare.length() && count == compare.length()) {
+                answer++;
+            }else if (compare.length() == target.length()) {
+                if (count == target.length() - 1 || count == target.length()) {
+                    answer++;
+                }
+            }else if (compare.length() == target.length() + 1) {
+                if (count == target.length()) {
+                    answer++;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
