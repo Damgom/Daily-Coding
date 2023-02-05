46d7a54f31f428555cad23c355c4c05a58e2c5c8
Damgom <wisewish1@gmail.com>
2023-02-05 20:07:20 +0900

    230205 binary search

---
+++ February/Boj_1166.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_1166 {
+    public static void main(String[] args) throws IOException {
+        /**
+         * @Boj_1166 선물
+         * @BinarySearch
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long n = Long.parseLong(st.nextToken());
+        long l = Long.parseLong(st.nextToken());
+        long w = Long.parseLong(st.nextToken());
+        long h = Long.parseLong(st.nextToken());
+        double low = 0;
+        double high = Math.max(l, Math.max(w, h));
+        double mid;
+        while (low < high) {
+            mid = (low + high) / 2;
+            if (high == mid || low == mid) {
+                break;
+            }
+            if ((long)(l / mid) * (long)(w / mid) * (long)(h / mid) < n) {
+                high = mid;
+            }else {
+                low = mid;
+            }
+        }
+        System.out.println(low);
+    }
+}
