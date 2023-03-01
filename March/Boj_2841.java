9c118de2aa08babbae4b53ff9c7199db5bbd961a
Damgom <wisewish1@gmail.com>
2023-03-01 13:55:22 +0900

    230301 stack

---
+++ March/Boj_2841.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Boj_2841 {
+    public static void main(String[] args) throws IOException {
+        /**
+         * @Boj_2841 외계인의 기타 연주
+         * @stack
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+        ArrayDeque<Integer>[] deques = new ArrayDeque[n+1];
+        for (int i = 0; i <= n; i++) {
+            deques[i] = new ArrayDeque<>();
+        }
+        int count = 0;
+        while (n-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int line = Integer.parseInt(st.nextToken());
+            int fret = Integer.parseInt(st.nextToken());
+            while (!deques[line].isEmpty()) {
+                if (deques[line].peek() < fret) {
+                    deques[line].push(fret);
+                }else if (deques[line].peek() > fret) {
+                    deques[line].pop();
+                }else break;
+                count++;
+            }
+            if (deques[line].isEmpty()) {
+                deques[line].push(fret);
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
