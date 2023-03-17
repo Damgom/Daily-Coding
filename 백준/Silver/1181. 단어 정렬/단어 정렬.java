dd61eaf198f4479f050036649ac330af98ed3391
Damgom <104135990+Damgom@users.noreply.github.com>
2023-03-17 19:16:09 +0900

    [Silver V] Title: 단어 정렬, Time: 376 ms, Memory: 26884 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            set.add(s);
+        }
+        List<String> list = new ArrayList<>(set);
+        list.sort((o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);
+            }
+            return o1.length() - o2.length();
+        });
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
