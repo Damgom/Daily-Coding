82cf2fa15330d50ab767852b0e2b73beee9811c9
Damgom <104135990+Damgom@users.noreply.github.com>
2023-03-22 14:39:17 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 128 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] graph = new int[n+1][n+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                graph[i][j] = 11111111;
+                if (i == j) {
+                    graph[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a][b] = graph[b][a] = 1;
+        }
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (graph[i][j] > graph[i][k] + graph[k][j]) {
+                        graph[i][j] = graph[i][k] + graph[k][j];
+                    }
+                }
+            }
+        }
+        int result = Integer.MAX_VALUE;
+        int index = 0;
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = 1; j <= n; j++) {
+                sum += graph[i][j];
+            }
+            if (result > sum) {
+                result = sum;
+                index = i;
+            }
+        }
+        System.out.println(index);
+    }
+}
