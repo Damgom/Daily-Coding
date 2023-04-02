7d909c11c4d69460fff8740e6c1a3fa15df45419
Damgom <104135990+Damgom@users.noreply.github.com>
2023-04-02 23:15:59 +0900

    [Silver II] Title: N과 M (12), Time: 280 ms, Memory: 23048 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static int[] answer;
+    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
+    static Set<String> set;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        visit = new boolean[n];
+        arr = new int[n];
+        answer = new int[m];
+        set = new LinkedHashSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0, 0);
+        set.forEach(System.out::println);
+    }
+    static void dfs(int start, int depth) {
+        if (depth == m) {
+            sb = new StringBuilder();
+            for (int i = 0; i < answer.length; i++) {
+                sb.append(answer[i]).append(" ");
+            }
+            set.add(sb.toString());
+            return;
+        }
+        for (int i = start; i < arr.length; i++) {
+//            if (!visit[i]) {
+//                visit[i] = true;
+//                answer[depth] = arr[i];
+//                dfs(i, depth + 1);
+//                visit[i] = false;
+//            }
+            answer[depth] = arr[i];
+            dfs(i, depth + 1);
+        }
+    }
+}
