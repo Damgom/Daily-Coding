e593c65f224c6e256586b139e5a016b6ed6e89fe
Damgom <104135990+Damgom@users.noreply.github.com>
2023-04-14 16:42:09 +0900

    [Silver I] Title: 영역 구하기, Time: 140 ms, Memory: 15288 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m, k;
+    static int[][] arr;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int count;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        arr = new int[m][n];
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            for (int j = y1; j < y2; j++) {
+                for (int k = x1; k < x2; k++) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] == 0) {
+                    count = 0;
+                    dfs(i, j);
+                    list.add(count);
+                }
+            }
+        }
+        Collections.sort(list);
+        System.out.println(list.size());
+        StringBuilder sb = new StringBuilder();
+        for (int c : list) {
+            sb.append(c).append(" ");
+        }
+        System.out.println(sb);
+    }
+    static void dfs(int x, int y) {
+        arr[x][y] = 1;
+        count++;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || ny < 0 || nx >= m || ny >= n) {
+                continue;
+            }
+            if (arr[nx][ny] == 0) {
+                dfs(nx, ny);
+            }
+        }
+    }
+}
