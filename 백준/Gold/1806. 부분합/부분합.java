44e386dd92b29a38d133a09c98534933d1277171
Damgom <104135990+Damgom@users.noreply.github.com>
2023-04-26 20:28:29 +0900

    [Gold IV] Title: 부분합, Time: 288 ms, Memory: 23792 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        int answer = Integer.MAX_VALUE;
+        while (true) {
+            if (sum >= s) {
+                sum -= arr[start];
+                answer = Math.min(answer, end - start);
+                start++;
+            }else if (end == n) {
+                break;
+            }else {
+                sum += arr[end++];
+            }
+        }
+        System.out.println(answer == Integer.MAX_VALUE ? 0 : answer);
+    }
+}
