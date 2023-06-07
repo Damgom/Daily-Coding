286f90810626d70b43b5a384fdc4520861e22134
Damgom <104135990+Damgom@users.noreply.github.com>
2023-06-07 21:53:01 +0900

    [Gold IV] Title: 문자열 폭발, Time: 568 ms, Memory: 62152 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String bomb = br.readLine();
+//        if (s.contains(bomb)) {
+//            s = s.replace(bomb, "");
+//        }
+//        while (s.contains(bomb)) {
+//            s = s.replace(bomb, "");
+//        }
+//        if (s.length() == 0) {
+//            System.out.println("FRULA");
+//        } else System.out.println(s);
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char temp = s.charAt(i);
+            stack.push(temp);
+            if (stack.size() >= bomb.length()) {
+                boolean flag = true;
+                for (int j = 0; j < bomb.length(); j++) {
+                    if (stack.get(stack.size() - bomb.length() + j) != bomb.charAt(j)) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    for (int j = 0; j < bomb.length(); j++) {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        if (stack.isEmpty()) {
+            sb.append("FRULA");
+        }else {
+            int size = stack.size();
+            for (int i = 0; i < size; i++) {
+                sb.append(stack.get(i));
+            }
+        }
+        System.out.println(sb);
+    }
+}
