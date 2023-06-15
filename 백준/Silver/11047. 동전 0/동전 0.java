ee9b8fbb6954d5b62dca6b58c3252b3fef071995
Damgom <104135990+Damgom@users.noreply.github.com>
2023-06-15 21:30:00 +0000

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        List<Integer> coin = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            int m = Integer.parseInt(br.readLine());
+            coin.add(m);
+        }
+        Collections.sort(coin, Collections.reverseOrder());
+        int count = 0;
+        for(int i = 0; i < coin.size(); i++) {
+            if(k > 0) {
+                int temp = k / coin.get(i);
+                count += temp;
+                k -= coin.get(i) * temp;
+            }
+        }
+        System.out.println(count);
+    }
+}
