ea408cc22af49970b18386474d5f862913edb456
Damgom <104135990+Damgom@users.noreply.github.com>
2023-12-01 09:37:11 +0900

    [Gold V] Title: 두 용액, Time: 400 ms, Memory: 29208 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int minValue = Integer.MAX_VALUE;
+        int start = 0;
+        int end = n - 1;
+        int num1 = 0;
+        int num2 = 0;
+        while (end > start) {
+            int sum = arr[start] + arr[end];
+            int abs = Math.abs(arr[start] + arr[end]);
+            if (minValue > abs) {
+                minValue = abs;
+                num1 = arr[start];
+                num2 = arr[end];
+            }
+            if (sum > 0) {
+                end--;
+            }else start++;
+        }
+        System.out.println(num1 + " " + num2);
+    }
+}
