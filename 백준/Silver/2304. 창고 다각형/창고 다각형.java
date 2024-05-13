f8ba9ac6beec99276294179e18a06890456bc8d7
Damgom <104135990+Damgom@users.noreply.github.com>
2024-05-13 18:39:26 +0900

    [Silver II] Title: 창고 다각형, Time: 132 ms, Memory: 14424 KB -BaekjoonHub

---
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[1001];
+        int max = Integer.MIN_VALUE;
+        int maxIndex = 0;
+        int lastIndex = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[a] = b;
+            if (b > max) {
+                max = b;
+                maxIndex = a;
+            }
+            if (a > lastIndex) {
+                lastIndex = a;
+            }
+        }
+        // 왼쪽
+        int left = 0;
+        int temp = arr[0];
+        for (int i = 0; i < maxIndex; i++) {
+            if (arr[i] > temp) {
+                temp = arr[i];
+            }
+            left += temp;
+        }
+        // 오른쪽
+        int right = 0;
+        int rightTemp = arr[lastIndex];
+        for (int i = lastIndex; i > maxIndex; i--) {
+            if (arr[i] > rightTemp) {
+                rightTemp = arr[i];
+            }
+            right += rightTemp;
+        }
+        int result = left + right + max;
+        System.out.println(result);
+    }
+}
