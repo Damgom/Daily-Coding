9112405c144d9b9bdd66726c9ce78991d69fa0c7
Damgom <104135990+Damgom@users.noreply.github.com>
2024-05-25 13:05:42 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 180 ms, Memory: 15804 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Deque<int[]> balloon = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder("1 ");
+
+        int firstBalloon = Integer.parseInt(st.nextToken());
+
+        for (int i = 2; i <= n; i++) {
+            balloon.offer(new int[]{i, Integer.parseInt(st.nextToken())});
+        }
+
+        while (!balloon.isEmpty()) {
+            if (firstBalloon > 0) {
+                for (int i = 1; i < firstBalloon; i++) {
+                    balloon.offer(balloon.pollFirst());
+                }
+                int[] temp = balloon.pollFirst();
+                firstBalloon = temp[1];
+                sb.append(temp[0]).append(" ");
+            } else {
+                for (int i = firstBalloon; i < -1; i++) {
+                    balloon.addFirst(balloon.pollLast());
+                }
+                int[] temp = balloon.pollLast();
+                firstBalloon = temp[1];
+                sb.append(temp[0]).append(" ");
+            }
+        }
+        System.out.println(sb);
+    }
+}
