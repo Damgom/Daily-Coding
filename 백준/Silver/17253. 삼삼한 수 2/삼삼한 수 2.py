a8ba11d6ae17ac7c9ebefb9ce03140eafcc74521
Damgom <104135990+Damgom@users.noreply.github.com>
2024-10-21 19:17:30 +0900

    [Silver III] Title: 삼삼한 수 2, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/17253. 삼삼한 수 2/삼삼한 수 2.py
@@ -0,0 +1,12 @@
+x = int(input())
+
+if x == 0:
+    print("NO")
+else:
+    while x:
+        if x % 3 > 1:
+            print("NO")
+            break
+        x //= 3
+    else:
+        print("YES")
