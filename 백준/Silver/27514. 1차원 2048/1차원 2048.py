af673ee1fcc562a36980adbff3c1af2635547482
Damgom <104135990+Damgom@users.noreply.github.com>
2024-11-07 22:13:43 +0900

    [Silver III] Title: 1차원 2048, Time: 156 ms, Memory: 56256 KB -BaekjoonHub

---
+++ 백준/Silver/27514. 1차원 2048/1차원 2048.py
@@ -0,0 +1,14 @@
+import math
+
+n = int(input())
+num = list(map(int, input().split()))
+p_num = [0 for _ in range(63)]
+for i in num:
+    if i:
+        p_num[int(math.log2(i))] += 1
+for i in range(62):
+    p_num[i + 1] += p_num[i] // 2
+for i in range(62, -1, -1):
+    if p_num[i]:
+        print(2 ** i)
+        break
